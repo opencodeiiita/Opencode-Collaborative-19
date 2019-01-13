cea0f3c57b60f72b8c3b71ce58ab6bf5a0e5611f
Ashish Agarowala <ashishagarwal2223@gmail.com>
2019-01-14 00:09:04 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
     document.getElementById("myBtn").style.display = "block";
   } else {
     document.getElementById("myBtn").style.display = "none";
