62bdae9864ab2a667471623b33d8efca34407755
Ashish Agarwal <ashishagarwal2225@gmail.com>
2019-01-13 19:35:27 +0530

    Added scroll button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 $(document).ready(function () {
     var mentors;
     var participants;
