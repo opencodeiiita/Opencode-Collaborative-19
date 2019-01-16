913fbf5afdcc2e09787d988f22f06658c99340d0
littleprodigy <45789780+littleprodigy@users.noreply.github.com>
2019-01-16 23:28:16 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
     $.getJSON(participantsJson, function (data) {
         participants = data.participants;
         participants.sort(function(a,b) {
-            return a.name.toLowerCase() > b.nametoLowerCase();
+            return a.name.toLowerCase() > b.name.toLowerCase();
         });
         $.each( participants, function( i, participant ) {
 
