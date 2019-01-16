f68bd0ad5686fd5a8752fdec3951f5077b12492f
Lalit Sharma <38866385+Rattlecode17sh@users.noreply.github.com>
2019-01-16 10:23:32 +0530

    Js preloader updated

--- js/main.js
+++ js/main.js
@@ -1,86 +1,202 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 $(document).ready(function () {
+
     var mentors;
     var participants;
     var projects;
 
-    var mentorsJson = 'data/mentors.json';
-    var participantsJson = 'data/participants.json';
-    var projectsJson = 'data/projects.json';
+    var mentorsJson = "data/mentors.json";
+    var participantsJson = "data/participants.json";
+    var projectsJson = "data/projects.json";
     
-  
     var preloader = document.getElementById('loading');
 
-     function myfunction(){
+    function myfunction(){
     preloader.style.display = "none";
-  }
+}
 
-    $.ajaxSetup({ beforeSend: function (xhr) {
-        if (xhr.overrideMimeType) {
-          xhr.overrideMimeType("application/json");
+    $.ajaxSetup({
+        beforeSend: function(xhr) {
+            if (xhr.overrideMimeType) {
+                xhr.overrideMimeType("application/json");
+            }
         }
-      }
     });
 
-    $.getJSON(mentorsJson, function (data) {
+    $.getJSON(mentorsJson, function(data) {
         mentors = data.mentors;
-        $.each( mentors, function( i, mentor ) {
-            var mentorDiv = "<div class='col-lg-2 col-sm-6 text-center mb-4'>" + 
-                            "<img class='img-fluid d-block mx-auto mb-4 profile-image' src=" + mentor.imageurl + " alt=''>" +
-                            "<h4>" +
-                            mentor.name +
-                            "</h4>" +
-                            "<p>" + mentor.about + "</p>" +
-                            "</div>"
-            
-            $('#mentors').append(mentorDiv);
+        $.each(mentors, function(i, mentor) {
+            if (/^ *$/.test(mentor.imageurl) || mentor.imageurl == "") {
+                //regex to check empty string or spaces
+
+                mentor.imageurl =
+                    "https://image.flaticon.com/icons/svg/1141/1141771.svg";
+                //Icon made by Freepik from www.flaticon.com *credits*
+            } else {
+                var url = mentor.imageurl;
+                console.log(url);
+
+                try {
+                    var http = new XMLHttpRequest();
+                    http.open("HEAD", url, false);
+                    http.send();
+
+                    if (http.status == 404) {
+                        mentor.imageurl =
+                            "https://image.flaticon.com/icons/svg/1141/1141771.svg";
+                    }
+                } catch (err) {}
+            }
+            var mentorDiv =
+                "<div class='col-lg-3 col-sm-6 text-center mb-4'>" +
+                "<div class='card participant-card'>" +
+                "<img class='card-img-top participant-img' src=" +
+                mentor.imageurl +
+                " alt=''>" +
+                "<div class='card-body'>" +
+                "<h4 class='card-title'>" +
+                mentor.name +
+                "</h4>" +
+                "<p class='card-text'>" +
+                mentor.about +
+                "</p>" +
+                "</div>" +
+                "<div class='social-media-links'>" +
+                "<a href=" +
+                mentor.facebook +
+                "><i class='fab fa-facebook-f'></i></a>" +
+                "<a href=" +
+                mentor.github +
+                "><i class='fab fa-github'></i></a>" +
+                "<a href=" +
+                mentor.twitter +
+                "><i class='fab fa-twitter'></i></a>" +
+                "</div>" +
+                "</div>" +
+                "</div>";
+
+            $("#mentors").append(mentorDiv);
         });
     });
 
-    $.getJSON(participantsJson, function (data) {
+    $.getJSON(participantsJson, function(data) {
         participants = data.participants;
-        $.each( participants, function( i, participant ) {
-            var participantDiv = "<div class='col-lg-2 col-sm-6 text-center mb-4'>" + 
-                                "<img class='img-fluid d-block mx-auto mb-4 profile-image' src=" + participant.imageurl + " alt=''>" +
-                                "<h4>" +
-                                participant.name +
-                                "</h4>" +
-                                "<h5>" +
-                                participant.college +
-                                "</h5>" +
-                                "<p>" + participant.about + "</p>" +
-                                "</div>"
-            
-            $('#participants').append(participantDiv);
+        $.each(participants, function(i, participant) {
+            if (
+                /^ *$/.test(participant.imageurl) ||
+                participant.imageurl == ""
+            ) {
+                //regex to check empty string or spaces
+
+                participant.imageurl =
+                    "https://image.flaticon.com/icons/svg/1141/1141771.svg";
+                //Icon made by Freepik from www.flaticon.com *credits*
+            } else {
+                var url = participant.imageurl;
+                console.log(url);
+
+                try {
+                    var http = new XMLHttpRequest();
+                    http.open("HEAD", url, false);
+                    http.send();
+
+                    if (http.status == 404) {
+                        participant.imageurl =
+                            "https://image.flaticon.com/icons/svg/1141/1141771.svg";
+                    }
+                } catch (err) {}
+            }
+            var participantDiv =
+                "<div class='col-lg-3 col-sm-6 text-center mb-4'>" +
+                "<div class='card participant-card'>" +
+                "<img class='card-img-top participant-img' src=" +
+                participant.imageurl +
+                " alt=''>" +
+                "<div class='card-body'>" +
+                "<h4 class='card-title'>" +
+                participant.name +
+                "</h4>" +
+                "<p class='card-text'>" +
+                participant.college +
+                "</p>" +
+                "<p class='card-text'>" +
+                participant.about +
+                "</p>" +
+                "</div>" +
+                "<div class='social-media-links'>" +
+                "<a href=" +
+                participant.facebook +
+                "><i class='fab fa-facebook-f'></i></a>" +
+                "<a href=" +
+                participant.github +
+                "><i class='fab fa-github'></i></a>" +
+                "<a href=" +
+                participant.twitter +
+                "><i class='fab fa-twitter'></i></a>" +
+                "</div>" +
+                "</div>" +
+                "</div>";
+
+            $("#participants").append(participantDiv);
         });
     });
 
-    $.getJSON(projectsJson, function (data) {
+    $.getJSON(projectsJson, function(data) {
         projects = data.projects;
-        $.each( projects, function( i, project ) {
-            var projectDiv = "<div class='col-lg-3 col-md-4 col-sm-6 portfolio-item'>" + 
-                                "<div class='card h-100'>" +
-                                    "<a href=" + project.github +"><img class='card-img-top' src=" + project.imageurl + " alt=''></a>" +
-                                    "<div class='card-body'>" +
-                                        "<h4 class='card-title'>" +
-                                            "<a href=" + project.github + ">" + project.name + "</a>" +
-                                        "</h4>" +
-                                        "<p class='card-text'>" + project.about + "</p>" +
-                                        "</hr>" +
-                                        "<h5>" +
-                                            "Mentors" +
-                                            "<p>" + project.mentors + "</p>" +
-                                        "</h5>" +
-                                        "</hr>" +
-                                        "<h5>" +
-                                            "Tech Stack" +
-                                            "<p>" + project.lang + "</p>" +
-                                        "</h5>" +
-                                    "</div>"
-                                "</div>"
-                            "</div>"
-            
-            $('#projects').append(projectDiv);
+        $.each(projects, function(i, project) {
+            var projectDiv =
+                "<div class='col-lg-3 col-md-4 col-sm-6 portfolio-item'>" +
+                "<div class='card h-100'>" +
+                "<a href=" +
+                project.github +
+                "><img class='card-img-top' src=" +
+                project.imageurl +
+                " alt=''></a>" +
+                "<div class='card-body'>" +
+                "<h4 class='card-title'>" +
+                "<a href=" +
+                project.github +
+                ">" +
+                project.name +
+                "</a>" +
+                "</h4>" +
+                "<p class='card-text'>" +
+                project.about +
+                "</p>" +
+                "</hr>" +
+                "<h5>" +
+                "Mentors" +
+                "<p>" +
+                project.mentors +
+                "</p>" +
+                "</h5>" +
+                "</hr>" +
+                "<h5>" +
+                "Tech Stack" +
+                "<p>" +
+                project.lang +
+                "</p>" +
+                "</h5>" +
+                "</div>";
+            ("</div>");
+            ("</div>");
+
+            $("#projects").append(projectDiv);
         });
     });
-
-});
+})
