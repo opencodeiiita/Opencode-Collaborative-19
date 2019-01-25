c3c6da6e1e21174c2bb0f7f808fb35631bd7c426
Archies11 <45824817+Archies11@users.noreply.github.com>
2019-01-25 14:41:06 +0530

    Create trailer.js

---
+++ js/trailer.js
@@ -0,0 +1,4 @@
+document.getElementById('trailer').addEventListener('click',function()
+{	document.querySelector('.modal').style.display='flex';});
+document.querySelector('.close').addEventListener('click',function()
+{	document.querySelector('.modal').style.display='none';});
