2d708b62d696c14a129acf7904da2d39e6ffb50b
Manthan Surkar <42006277+thesmallstar@users.noreply.github.com>
2019-02-13 14:34:02 +0530

    Update Commits in repos.m

--- opencode19-visualizations/thesmallstar/Commits in repos.m
+++ opencode19-visualizations/thesmallstar/Commits in repos.m
@@ -1,11 +1,23 @@
-t = readtable('./data.csv');
- plot(t.Weeks,t.OC,'LineWidth',3);
+url = ['https://api.github.com/repos/GeekHaven/Leave-Application-Portal/stats/participation'];
+S = webread(url);
+plot(S.all,'linewidth',3);
+xlim([47 52]);
+hold on;
+url = ['https://api.github.com/repos/opencodeiiita/sassy-css/stats/participation'];
+S = webread(url);
+plot(S.all,'linewidth',3);
+hold on;  
+url = ['https://api.github.com/repos/opencodeiiita/HackerSkills/stats/participation'];
+S = webread(url);
+plot(S.all,'linewidth',3);
+hold on;
+url = ['https://api.github.com/repos/opencodeiiita/Opencode-Collaborative-19/stats/participation'];
+S = webread(url);
+plot(S.all,'linewidth',3);
+hold on;
+xlabel("Week- 48: week 1");
+ylabel("Number of commits");
 
-hold on;
-plot(t.Weeks,t.LAP,'LineWidth',3);
-hold on;
-plot(t.Weeks,t.SCSS,'LineWidth',3);
-hold on;
-plot(t.Weeks,t.HS,'LineWidth',3);
-ylabel("Commits in the repository");
-xlabel("Weeks");
+
+
+
